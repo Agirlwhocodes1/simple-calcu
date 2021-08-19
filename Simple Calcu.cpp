935ceb2b364f24e3ec5792f8870dfc47d55f755c
Bahle <88790002+Agirlwhocodes1@users.noreply.github.com>
2021-08-20 01:09:44 +0200

    Add files via upload

---
+++ Simple Calcu.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+//Declare variables for asked values
+ float var1, var2;
+ char operation;
+ float result;
+
+ //prompts user for info,user inputs data
+cout << "Calculator" << endl;
+cout<< "==================="<< endl<<endl;
+
+cout << "Please enter the first value: " << endl<< endl;
+cin >> var1;
+cout<< "======================================"<<endl<<endl;
+cout << "Please enter the second value: "<< endl;
+cin >> var2;
+cout << "=============================="<< endl<<endl;
+cout << "Please enter the operation required: "<< endl;
+cin >> operation;
+
+//if statements for results ,displays results
+if ( operation == '+'){
+    result = var1+var2;
+    cout <<"The sum of  "<< var1<< " and " <<var2<< " is " << result << " ,Thank you"<<endl;
+}
+else if (operation == '-'){
+    result = var1-var2;
+    cout <<"The sum of  "<< var1<< " and " <<var2<< " is " << result << " ,Thank you"<<endl;
+}
+ else if( operation == '*'){
+    result= var1*var2;
+    cout <<"The sum of  "<< var1<< " and " <<var2<< " is " << result << " ,Thank you"<<endl;
+
+}
+else {
+    cout << "invalid" << endl;
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     return 0;
+
+}
